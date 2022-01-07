aadba400ea312bd070edc0311775ee956302e99a
schollenberger <w.schollenberger@gmail.com>
2022-01-07 18:50:08 +0100

    bug fixes in ra_strommessung1.py

--- ina219/ra_strommessung1.py
+++ ina219/ra_strommessung1.py
@@ -6,16 +6,25 @@ MAX_STROMSTAERKE = 0.4
 ina = INA219(SHUNT_OHM, MAX_STROMSTAERKE)
 ina.configure(ina.RANGE_16V, ina.GAIN_1_40MV)
 
-dev read_ina219():
+def read_ina219():
     try:
-        Uges = ina.voltage() + ina.shunt_voltage()/1000
+        Ushunt = ina.shunt_voltage()
+        Uina = ina.voltage()
+        Uges = Uina + Ushunt/1000
         print('Ubat   : {0:0.2f} V'.format(Uges))
         print('Iges   : {0:0.2f} mA'.format(ina.current()))
         print('Pges   : {0:0.2f} mW'.format(ina.power()))
-        print('UShunt : {0:0.2f} V'.format(ina.shunt_voltage()))
+        print('UShunt : {0:0.2f} mV'.format(Ushunt))
+        print
     except DeviceRangeError as e:
-        print("Stromstärke zu hoch.")
+        print("Stromstaerke zu hoch.")
 
-while 1:
-    read_ina219()
-    sleep(5)
+try:
+    while 1:
+        read_ina219()
+        sleep(5)
+
+except KeyboardInterrupt:
+    pass
+    print
+    print("That's all folks ...")
