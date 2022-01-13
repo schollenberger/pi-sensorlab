f8ca125ddf440858d204a586c939e4dbb389b33a
schollenberger <w.schollenberger@gmail.com>
2022-01-13 18:43:17 +0100

    lcd-simple.py 1st working version

--- LCD-Module/lcd-simple.py
+++ LCD-Module/lcd-simple.py
@@ -7,7 +7,22 @@ from Adafruit_CharLCD import Adafruit_CharLCD
 lcd = Adafruit_CharLCD(rs=21, en=20, d4=16, d5=12, d6=7, d7=8,
                        cols=16, lines=2)
 
-lcd.message('Hello')
-sleep(3)
-lcd.message('Hello')
+
+def print_display(txt):
+     print txt
+     lcd.message(txt)
+
+
+
 lcd.clear()
+sleep(1)
+print_display("Hello")
+sleep(3)
+print_display(' World!')
+sleep(3)
+print_display("\nSecond line..")
+sleep(3)
+lcd.clear()
+print_display("Bye!")
+
+#lcd.clear()
