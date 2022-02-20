7723acd36a7186dec5d7a2f36a91a3fe6b7ea88e
schollenberger <w.schollenberger@gmail.com>
2022-02-20 15:49:26 +0100

    add simple programs to switch the dispaly off/on via the Adafruit Oled module

---
+++ oled-display/oled-adafruit-off.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# This example is for use on (Linux) computers that are using CPython with
+# Adafruit Blinka to support CircuitPython libraries. CircuitPython does
+# not support PIL/pillow (python imaging library)!
+
+from board import SCL, SDA
+import busio
+import adafruit_ssd1306
+
+
+# Create the I2C interface.
+i2c = busio.I2C(SCL, SDA)
+
+# Create the SSD1306 OLED class.
+# The first two parameters are the pixel width and pixel height.  Change these
+# to the right size for your display!
+disp = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
+
+# Clear display.
+disp.poweroff()
+print("Display turned off...")
