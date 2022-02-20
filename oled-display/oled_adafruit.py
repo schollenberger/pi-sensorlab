45faebb7f04ec38a6b3bd881bbcd1c227894fd2e
schollenberger <w.schollenberger@gmail.com>
2022-02-20 13:46:52 +0100

    OledLuma/OledAdafruit classes: Add txt paramter to greetings, add hide(), show() methods plus test code.

--- oled-display/oled_adafruit.py
+++ oled-display/oled_adafruit.py
@@ -2,47 +2,84 @@
 # -*- coding: utf-8 -*-
 #
 # This module defines the custom OLED dDisplay object based on the
-# Adafruit OLED module. It only works with the SSD1306 controller.
+# Adafruit OLED module. It works for the SSD1306 only.
 #
-# It uses the luma.core.reder.canvas object to display text.
+# The display stays on after program exit if not cleared manually.
 #
-from luma.core.interface.serial import i2c
-from luma.core.render import canvas
-from luma.oled.device import sh1106, ssd1306
-from PIL import ImageFont, ImageDraw, Image
+from board import SCL, SDA
+import busio
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
 
 class OledAdafruit:
-    def __init__(self, device,font = None):
+    def __init__(self, device, font = None):
         self.device = device
         if not font:
             self.oled_font=ImageFont.truetype('DejaVuSansMono-Oblique.ttf', 10)
         else:
             self.oled_font=font
 
-    def greeting(self):
-        with canvas(self.device) as draw:
-            draw.rectangle(self.device.bounding_box, outline = "white", fill = "black")
-            draw.text((10, 10), "OLED-Display", font = self.oled_font, fill = "white")
+        self.device.fill(0)
+        self.device.show()
+
+    def greeting(self, txt = None):
+        if not txt:
+            greeting_text = "OLED-Display"
+        else:
+            greeting_text = txt
+
+        img = Image.new("1", (self.device.width, self.device.height))
+        draw = ImageDraw.Draw(img)
+        draw.rectangle((0,0,self.device.width-1, self.device.height-1), outline = "white", fill = "black")
+        draw.text((10, 10), greeting_text, font = self.oled_font, fill = "white")
+
+        self.device.image(img)
+        self.device.show()
+
 
     def display_text(self, txt):
-        with canvas(self.device) as draw:
-#            draw.rectangle(self.device.bounding_box, outline = "white", fill = "black")
-            draw.text((5, 5), txt, font = self.oled_font, fill = "white")
+        img = Image.new("1", (self.device.width, self.device.height))
+        draw = ImageDraw.Draw(img)
+        draw.text((5, 5), txt, font = self.oled_font, fill = "white")
+        self.device.image(img)
+        self.device.show()
+
+    def clear(self):
+        self.device.fill(0)
+        self.device.show()
+
+    def hide(self):
+        self.device.poweroff()
+
+    def show(self):
+        self.device.poweron()
 
 
 if __name__ == "__main__":
      from time import sleep
-
-     i2cdev = i2c(port=1, address=0x3C)
-
-     oled_dev = sh1106(i2cdev)
-#     oled_dev = ssd1306(i2cdev)
+     i2cdev = busio.I2C(SCL, SDA)
+     ada_dev = adafruit_ssd1306.SSD1306_I2C(128, 64, i2cdev)
 
      myfont=ImageFont.truetype('DejaVuSansMono-Oblique.ttf', 15)
-     oled = OledLuma(oled_dev, myfont)
+     oled = OledAdafruit(ada_dev, myfont)
      oled.greeting()
      print("Module works correctly if you can see the message 'OLED-Display' on your OLED display.")
-     print("Waiting 5 sec before exiting - which should clear the display.")
+     print("Waiting 5 sec before hiding the display...")
      sleep(5)
-     # Note, deleting the display device doesn't clear the display but exiting the program does.
+     print("...hide...")
+     oled.hide()
+     print("Waiting 5 sec before showing the display again ...")
+     sleep(5)
+     print("...show...")
+     oled.show()
+     print("Waiting 5 sec and then clear the display for 5 sec...")
+     sleep(5)
+     print("Clear...")
+     oled.clear()
+     sleep(5)
+     print("Greeting...")
+     oled.greeting()
+     print("Waiting 5 sec before clearing the display and exiting.")
+     sleep(5)
+     oled.clear()
      print("That's all...")
--- oled-display/oled_luma.py
+++ oled-display/oled_luma.py
@@ -20,29 +20,57 @@ class OledLuma:
         else:
             self.oled_font=font
 
-    def greeting(self):
+    def greeting(self, txt = None):
+        if not txt:
+            greeting_text = "OLED-Display"
+        else:
+            greeting_text = txt
+
         with canvas(self.device) as draw:
             draw.rectangle(self.device.bounding_box, outline = "white", fill = "black")
-            draw.text((10, 10), "OLED-Display", font = self.oled_font, fill = "white")
+            draw.text((10, 10), greeting_text, font = self.oled_font, fill = "white")
 
     def display_text(self, txt):
         with canvas(self.device) as draw:
 #            draw.rectangle(self.device.bounding_box, outline = "white", fill = "black")
             draw.text((5, 5), txt, font = self.oled_font, fill = "white")
 
+    def clear(self):
+         self.device.clear()
+
+    def hide(self):
+         self.device.hide()
+
+    def show(self):
+         self.device.show()
+
 
 if __name__ == "__main__":
      from time import sleep
 
      i2cdev = i2c(port=1, address=0x3C)
-
-     oled_dev = sh1106(i2cdev)
-#     oled_dev = ssd1306(i2cdev)
+     luma_dev = sh1106(i2cdev)
+#     luma_dev = ssd1306(i2cdev)
 
      myfont=ImageFont.truetype('DejaVuSansMono-Oblique.ttf', 15)
-     oled = OledLuma(oled_dev, myfont)
+     oled = OledLuma(luma_dev, myfont)
      oled.greeting()
      print("Module works correctly if you can see the message 'OLED-Display' on your OLED display.")
+     print("Waiting 5 sec before hiding the display...")
+     sleep(5)
+     print("...hide...")
+     oled.hide()
+     print("Waiting 5 sec before showing the display again ...")
+     sleep(5)
+     print("...show...")
+     oled.show()
+     print("Waiting 5 sec and then clear the display for 5 sec...")
+     sleep(5)
+     print("Clear...")
+     oled.clear()
+     sleep(5)
+     print("Greeting...")
+     oled.greeting()
      print("Waiting 5 sec before exiting - which should clear the display.")
      sleep(5)
      # Note, deleting the display device doesn't clear the display but exiting the program does.
